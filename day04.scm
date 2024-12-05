dc2d61ff735af1b3a61b0ad5d941cec7644ad23f
Juan Marín Noguera <juan@mnpi.eu>
2024-12-05 12:07:23 +0100

    Put all of day 04 in the same file
    In the beginning I thought of using modular components, but this would
    mean that I would have to retest previous days if I want to make a
    change to the implementation.  Thus, for learning purposes, I prefer
    to keep everything in one file.

--- day04.scm
+++ day04.scm
@@ -1,9 +1,24 @@
 (define-module (day04)
   #:use-module (ice-9 rdelim)
   #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-9)
   #:use-module (srfi srfi-26)
-  #:use-module (srfi srfi-41)
-  #:use-module (util))
+  #:use-module (srfi srfi-41))
+
+(define-record-type <point> (point i j) point?
+                    (i point-i)
+                    (j point-j))
+
+(define (square i1 j1 i2 j2)
+  (define (row i)
+    (stream-map (cut point i <>) (stream-range j1 j2)))
+  (stream-concat (stream-map row (stream-range i1 i2))))
+
+(define (array-point arr pt)
+  (array-ref arr (point-i pt) (point-j pt)))
+
+(define (point-add pt i j)
+  (point (+ (point-i pt) i) (+ (point-j pt) j)))
 
 (define* (read-input #:optional (port (current-input-port)))
   (define (read-lines acc)
